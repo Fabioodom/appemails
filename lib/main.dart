fe010e3a6193edc99484a1f3e68aaf6acdae4fc3
Fabioodom <fabiodominguez2003@gmail.com>
2025-05-12 17:40:22 +0200

    prueba10

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,34 @@
 // lib/main.dart
-//prueba
+
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart'   show kIsWeb;
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';     // <<< archivo generado
+import 'package:firebase_auth_web/firebase_auth_web.dart';
+import 'firebase_options.dart';
 import 'routes.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // 1) Inicializa Firebase con la configuración de firebase_options.dart
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(MyApp());
+
+  // 2) Solo en web, registra el plugin de Firebase Auth
+  if (kIsWeb) {
+    // 'webPluginRegistrar' viene de flutter_web_plugins
+    FirebaseAuthWeb.registerWith(webPluginRegistrar);
+  }
+
+  // 3) Arranca tu aplicación
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
