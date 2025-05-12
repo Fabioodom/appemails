6ff7fec0161c41f31ff44768b70aaa5960c1b25e
Fabioodom <fabiodominguez2003@gmail.com>
2025-05-12 17:54:41 +0200

    prueba12

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1847096947"
+    serviceWorkerVersion: "2497032946"
   }
 });
