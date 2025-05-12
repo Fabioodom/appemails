8f5d94a42ea4186aa8aa3544ac3495b75cdedcdd
Fabioodom <fabiodominguez2003@gmail.com>
2025-05-12 18:51:25 +0200

    rutas arreglo

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart'     show kIsWeb;
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
-// implementaciones web de los plugins
+// Implementaciones web de Firebase
 import 'package:firebase_core_web/firebase_core_web.dart';
 import 'package:firebase_auth_web/firebase_auth_web.dart';
 import 'package:cloud_firestore_web/cloud_firestore_web.dart';
@@ -12,25 +12,25 @@ import 'package:cloud_firestore_web/cloud_firestore_web.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-
+// Tus pantallas
 import 'screens/email_form_screen.dart';
-import 'screens/admin_screen.dart';
 import 'screens/privacy_screen.dart';
 import 'screens/login_screen.dart';
+import 'screens/admin_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   if (kIsWeb) {
-    // 1) registra las implementaciones web
+    // 1) Registra las implementaciones web
     FirebaseCoreWeb.registerWith(webPluginRegistrar);
     FirebaseAuthWeb.registerWith(webPluginRegistrar);
     FirebaseFirestoreWeb.registerWith(webPluginRegistrar);
-    // 2) finalmente, instala el manejador de mensajes
+    // 2) Instala el manejador de mensajes
     webPluginRegistrar.registerMessageHandler();
   }
 
-  // 3) inicializa tu FirebaseApp normal
+  // 3) Inicializa FirebaseApp
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
@@ -46,15 +46,17 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Mi App Mail',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue),
+      // Al arrancar, va primero a EmailFormScreen
       initialRoute: '/',
-       routes: {
-        'privacy': (context) => PrivacyScreen(),
-        // Ruta de login
+      routes: {
+        // Ruta por defecto: formulario de email
+        '/': (context) => EmailFormScreen(),
+        // Pantalla de privacidad
+        '/privacy': (context) => PrivacyScreen(),
+        // Login de admin
         '/login': (context) => LoginScreen(),
-        // Ruta de panel de admin
+        // Panel de admin
         '/admin': (context) => AdminScreen(),
       },
     );
