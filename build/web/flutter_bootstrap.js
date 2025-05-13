04d0bd86ca473499a04b7df78eb0ac063ad902a4
Fabioodom <fabiodominguez2003@gmail.com>
2025-05-13 12:27:51 +0200

    icon

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2592404028"
+    serviceWorkerVersion: "2663746728"
   }
 });
